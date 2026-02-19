84b7cd09930e23ab20bdc1273dacf614015cefb2
Prabhat Kumar Singh <pprabhatsingh55555@gmail.com>
2026-02-20 00:45:54 +0530

    Refactor CORS middleware to use environment variable
    Updated CORS origin to use environment variable.

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://pharmacogenomicriskpredictionsystem.netlify.app',
+  origin: process.env.CORS_ORIGIN || 'https://pharmacogenomicriskpredictionsystem.netlify.app',
   credentials: true
 }));
 
@@ -71,6 +71,7 @@ process.on('SIGTERM', () => {
 });
 
 module.exports = app;
+
 
 
 
