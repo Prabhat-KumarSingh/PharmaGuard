3aa3cab753bea99edf8342bfb49c1de2831f6dde
Prabhat Kumar Singh <pprabhatsingh55555@gmail.com>
2026-02-20 00:44:55 +0530

    Refactor server.js for cleaner structure

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'https://pharmacogenomicriskpredictionsystem.netlify.app',
+  origin: 'https://pharmacogenomicriskpredictionsystem.netlify.app',
   credentials: true
 }));
 
@@ -71,5 +71,6 @@ process.on('SIGTERM', () => {
 });
 
 module.exports = app;
+
 
 
