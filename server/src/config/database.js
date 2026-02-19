147bb1e1e6d4a241d7b36af60e677e059d5636cc
Prabhat Kumar Singh <pprabhatsingh55555@gmail.com>
2026-02-20 03:15:34 +0530

    Refactor database connection code in database.js

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://pprabhatsingh55555_db_user:Pks%401234@cluster0.z4vywtr.mongodb.net/pharmacogenomics?retryWrites=true&w=majority';
+    const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://pprabhatsingh55555_db:Pks%401234@cluster0.z4vywtr.mongodb.net/pharmacogenomics?retryWrites=true&w=majority';
     
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
@@ -18,6 +18,7 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
 
 
 
